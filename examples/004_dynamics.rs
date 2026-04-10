b472019b908af5c991925bcc194ff8c6b159496e
Pome <unclepome.dev@gmail.com>
2026-04-10 18:23:57 +0900

    fmt

--- examples/004_dynamics.rs
+++ examples/004_dynamics.rs
@@ -6,7 +6,10 @@ use houdini_ramen::sop::{
     SopAttribvop, SopAttribvopBindclass, SopAttribvopInnerExt, SopScatter, SopSolver,
     SopSolverInnerExt, SopTestgeometryRubbertoy,
 };
-use houdini_ramen_vop::{VopAdd, VopConstant, VopGeometryvopglobal, VopGeometryvopglobalOutputs, VopKinefxJointangle, VopMultiply};
+use houdini_ramen_vop::{
+    VopAdd, VopConstant, VopGeometryvopglobal, VopGeometryvopglobalOutputs, VopKinefxJointangle,
+    VopMultiply,
+};
 
 const TIMESTEP: f32 = 1.0_f32 / 24.0_f32;
 
@@ -45,9 +48,8 @@ fn main() {
                     .set_input(in1.out_v())
                     .set_input_at(1, &const1),
             );
-            let dummy = vop_graph.add(VopKinefxJointangle::new("dummy")
-                .set_input_name_pt(&const1)
-            );
+            let _dummy =
+                vop_graph.add(VopKinefxJointangle::new("dummy").set_input_name_pt(&const1));
 
             let add1 = vop_graph.add(
                 VopAdd::new("add1")
